8d53fb189e38985cf9620753ea4fb70d0d6f83ea
jamesjohnson280 <james.johnson280@gmail.com>
2018-08-09 11:27:13 -0400

    Remove debug logging

--- sw.js
+++ sw.js
@@ -8,10 +8,8 @@ var filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
@@ -19,7 +17,6 @@ self.addEventListener('install', function(e) {
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
