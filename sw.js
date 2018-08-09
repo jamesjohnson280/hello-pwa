25a14f038a011e4643a8133be7ace1f0b8ae4a41
jamesjohnson280 <james.johnson280@gmail.com>
2018-08-09 14:07:23 -0400

    Change app urls to work on domain subfolder

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
